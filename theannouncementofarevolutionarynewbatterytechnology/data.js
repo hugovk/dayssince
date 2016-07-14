538a44347fe6dfa2723dde9e14a1d8dd2a026c71
Hugo <hugovk@users.noreply.github.com>
2016-07-14 11:02:47 +0300

    Days since updating theannouncementofarevolutionarynewbatterytechnology: 0

--- theannouncementofarevolutionarynewbatterytechnology/data.js
+++ theannouncementofarevolutionarynewbatterytechnology/data.js
@@ -1,2 +1,2 @@
-var last_date = new Date("22 April 2016");
-var last_url = "https://twitter.com/Alby/status/723439839681499136";
+var last_date = new Date("11 July 2016");
+var last_url = "https://twitter.com/Alby/status/711878491093012480";
