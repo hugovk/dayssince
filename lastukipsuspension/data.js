2833fe07d7908d3237142d8b862c2cbceb48df5c
Hugo <hugovk@users.noreply.github.com>
2016-10-11 20:53:11 +0300

    Days since updating last suspension: 0
    > A key figure in UKIP’s Peterborough branch says he has been suspended for “speaking out against elements of racism, cronyism, corruption and hypocrisy in our party.”

--- lastukipsuspension/data.js
+++ lastukipsuspension/data.js
@@ -1,2 +1,2 @@
-var last_date = new Date("23 March 2016");
-var last_url = "http://www.pinknews.co.uk/2016/03/23/ukips-suzanne-evans-suspended-for-criticising-gay-cure-candidate";
+var last_date = new Date("23 September 2016");
+var last_url = "http://www.peterboroughtoday.co.uk/our-region/peterborough/key-ukip-peterborough-figure-suspended-for-speaking-out-against-racism-1-7594382";
