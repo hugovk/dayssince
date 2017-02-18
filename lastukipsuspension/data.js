e8c5fa47b4ad4b5fab21d83966b431d6719715b1
Hugo <hugovk@users.noreply.github.com>
2017-02-18 08:27:50 +0200

    Days since updating last suspension: 0
    148 -> 4

--- lastukipsuspension/data.js
+++ lastukipsuspension/data.js
@@ -1,2 +1,2 @@
-var last_date = new Date("23 September 2016");
-var last_url = "http://www.peterboroughtoday.co.uk/our-region/peterborough/key-ukip-peterborough-figure-suspended-for-speaking-out-against-racism-1-7594382";
+var last_date = new Date("14 February 2017");
+var last_url = "http://www.bbc.co.uk/news/uk-england-bristol-38971604";
