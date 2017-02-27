2f47d447fb5c910b7145267f02118bbd0572db11
Hugo <hugovk@users.noreply.github.com>
2017-02-27 09:53:25 +0200

    Days since updating: 0
    "Finns Party sacks Tampere chapter chair convicted of ethnic incitement"
    ...
    "Slunga-Poutsalo said that Kiemunki's dismissal arose from irregularities with her travel invoices, however she noted that it was just one one of many factors that led to the decision."

--- lasttruefinnssuspension/data.js
+++ lasttruefinnssuspension/data.js
@@ -1,2 +1,2 @@
-var last_date = new Date("3 April 2016");
-var last_url = "http://yle.fi/uutiset/mondays_papers_panama_papers_local_finns_party_vice_chair_sacked_world_champ_dreams_knocked_out/8785280";
+var last_date = new Date("26 February 2017");
+var last_url = "http://yle.fi/uutiset/osasto/news/finns_party_sacks_tampere_chapter_chair_convicted_of_ethnic_incitement/9480783";
