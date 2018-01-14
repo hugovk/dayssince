dd2b1ab15c573b94d447c9faefde9b60a451efd4
Hugo <hugovk@users.noreply.github.com>
2018-01-14 10:00:21 +0200

    Days since last UKIP suspension: 0

--- lastukipsuspension/data.js
+++ lastukipsuspension/data.js
@@ -1,2 +1,2 @@
-var last_date = new Date("20 May 2017");
-var last_url = "https://www.theguardian.com/politics/2017/may/20/ukip-candidate-suspended-for-sending-racist-tweets";
+var last_date = new Date("14 January 2018");
+var last_url = "https://www.bbc.com/news/uk-politics-42679187";
