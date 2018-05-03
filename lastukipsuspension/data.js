1014c3e419f93446ca5477d8da51f5d79076d3b4
Hugo <hugovk@users.noreply.github.com>
2018-05-03 08:23:36 +0300

    Days since last UKIP suspension: 0

--- lastukipsuspension/data.js
+++ lastukipsuspension/data.js
@@ -1,2 +1,2 @@
-var last_date = new Date("14 January 2018");
-var last_url = "https://www.bbc.com/news/uk-politics-42679187";
+var last_date = new Date("2 May 2018");
+var last_url = "https://www.theguardian.com/politics/2018/may/02/ukip-michelle-brown-suspended-welsh-assembly-racial-abuse-chuka-umunna";
